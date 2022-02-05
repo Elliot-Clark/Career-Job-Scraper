c06063127bb692e5db7675919724585320afe799
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-04 23:17:36 -0700

    POST to Backend added

--- client/src/components/clientInput/clientInput.js
+++ client/src/components/clientInput/clientInput.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
 import './clientInput.css';
+import axios from 'axios';
 
 
 class ClientInput extends Component {
- 
     constructor() {
         super()
         this.state = {
             scraperData: "",
+            mainSearchQuery: "Front End Web Developer",
+            city: "Phoenix",
+            filterWords: "Web",
+            bannedWords: "Fullstack",
+            Companies: "Apple etc..."
+
         }
     }
 
     jobSearchInit = () => {
         fetch('/api/test')
-        .then(res => res.json())
+        .then(res => res.text())
         .then(scraperData => this.setState({scraperData}, () => console.log('Scraper Data fetched...')))
     }
+
+    testSend = () => {
+        if (!document.getElementById("jobSearchInput").value) {
+            return
+        }
+        axios.post('/aaa', {}, {
+            params: {
+                One: 'feaffaf',
+                Two: 'lnhy;thktr',
+                Three: document.getElementById("jobSearchInput").value
+            }
+          });
+    }
+
 
     render() {
         return (
             <div>
                 <h2>Data:</h2>
                 <button onClick={this.jobSearchInit}>Fetch Data</button>
+                <button onClick={this.testSend}>Test Send</button>
                 {this.state.scraperData}
+
+                <div id="inputArea">
+                    <input type="text" id="jobSearchInput" name="jobSearchInput" placeholder='Enter Job Here'></input>
+                    <input type="button" value="Submit" onClick={this.testSend}></input>
+                </div> 
             </div>
         );
     }
--- server.js
+++ server.js
@@ -4,16 +4,23 @@ const puppeteer = require('puppeteer');
 const app = express();
 
 
+app.post('/aaa', function(req, res) {
+	let yes = req.query
+	console.log(yes);
+  });
+
+
+
 app.get('/api/test', (req, res) => {
 	(async () => {
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage(); 
-		await page.goto("https://elliot-clark.com/", {
+		await page.goto("https://www.amazon.jobs/en/search?offset=0&result_limit=10&sort=relevant&job_type%5B%5D=Full-Time&country%5B%5D=USA&state%5B%5D=Arizona&distanceType=Mi&radius=24km&latitude=&longitude=&loc_group_id=&loc_query=&base_query=front%20end&city=&country=&region=&county=&query_options=&", {
 			waitUntil: "networkidle2",
 		  });
-		let data = await page.$$eval('section div li', links => {
+		let data = await page.$$eval('.job-title', links => {
 			links = links.map(element => element.textContent)
-			return links
+			return links[0]
 		});
 		console.log(data);
 		res.json(data);
