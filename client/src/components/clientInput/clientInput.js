82cda8f2ca04df38094a056d0ecd47349603d00b
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-06 15:21:15 -0700

    Updated for new purpose

--- client/src/components/clientInput/clientInput.js
+++ client/src/components/clientInput/clientInput.js
@@ -7,9 +7,11 @@ class ClientInput extends Component {
     constructor() {
         super()
         this.state = {
-            scraperData: "",
-            mainSearchQuery: "Front End Web Developer",
-            city: "Phoenix",
+            jobTitleSearch: "Front End Web Developer",
+            city: "",
+            USstate: "Arizona",
+            country: "",
+            resultsPerCompany: 5,
             filterWords: "Web",
             bannedWords: "Fullstack",
             Companies: "Apple etc..."
@@ -17,37 +19,44 @@ class ClientInput extends Component {
         }
     }
 
-    jobSearchInit = () => {
-        fetch('/api/test')
-        .then(res => res.text())
-        .then(scraperData => this.setState({scraperData}, () => console.log('Scraper Data fetched...')))
-    }
+    // jobSearchInit = () => {
+    //     fetch('/api/test')
+    //     .then(res => res.text())
+    //     .then(scraperData => this.setState({scraperData}, () => console.log('Scraper Data fetched...')))
+    // }
 
-    testSend = () => {
+
+    dataSend = () => {
         if (!document.getElementById("jobSearchInput").value) {
             return
         }
-        axios.post('/aaa', {}, {
+        this.props.toggleReplaceJobs();
+        axios.post('/jobSearch', {}, {
             params: {
-                One: 'feaffaf',
-                Two: 'lnhy;thktr',
-                Three: document.getElementById("jobSearchInput").value
+                jobTitleSearch: document.getElementById("jobSearchInput").value,
+                city: document.getElementById("cityInput").value,
+                country: document.getElementById("countryInput").value,
+                USstate: document.getElementById("stateInput").value,
+                resultsPerCompany: document.getElementById("numberOfResults").value,
             }
-          });
+        }).then(response => {
+            console.log(response.data);
+            this.props.handleCallBack(response.data);
+        });
     }
 
 
     render() {
         return (
-            <div>
-                <h2>Data:</h2>
-                <button onClick={this.jobSearchInit}>Fetch Data</button>
-                <button onClick={this.testSend}>Test Send</button>
-                {this.state.scraperData}
-
-                <div id="inputArea">
-                    <input type="text" id="jobSearchInput" name="jobSearchInput" placeholder='Enter Job Here'></input>
-                    <input type="button" value="Submit" onClick={this.testSend}></input>
+            <div id="inputArea">
+                <h2>Careers Job Search:</h2>
+                <div>
+                    <input type="text" id="jobSearchInput" name="jobSearchInput" placeholder='Enter Job Here' defaultValue={"Front End"}></input>
+                    <input type="text" id="cityInput" name="cityInput" placeholder='City'></input>
+                    <input type="text" id="countryInput" name="countryInput" placeholder='Country'></input>
+                    <input type="text" id="stateInput" name="stateInput" placeholder='State' defaultValue={"Arizona"}></input>
+                    <input type="number" id="numberOfResults" name="numberOfResults" min="1" max="10" defaultValue={5}></input>
+                    <input type="button" value="Submit" onClick={this.dataSend}></input>
                 </div> 
             </div>
         );
