4147ba4418525d4158037f063db843ecbdefc12d
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-06 15:20:29 -0700

    Will now process input from multiple backend responses

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,62 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ClientInput from './components/clientInput/clientInput';
+import JobDisplay from './components/jobDisplay/jobDisplay';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      replaceJobs: false,
+      companies: '',
+      searchlURLs: '',
+      jobs: []
+    }
+  }
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <ClientInput />
-      </header>
-    </div>
-  );
+  toggleReplaceJobs = () => {
+      this.setState({ replaceJobs: true});
+  }
+
+  handleCallBack = (fetchedJobResults) => {
+    if (!this.state.replaceJobs) {
+      this.setState({ 
+        companies: [this.state.companies, fetchedJobResults[0]],
+        searchlURLs: [this.state.searchlURLs, fetchedJobResults[1]],
+        jobs: [this.state.jobs, fetchedJobResults.slice(2)]
+      }, () => {
+      });
+    }
+    if (this.state.replaceJobs) {
+      this.setState({ 
+        replaceJobs: false,
+        companies: [fetchedJobResults[0]],
+        searchlURLs: [fetchedJobResults[1]],
+        jobs: fetchedJobResults.slice(2)
+      }, () => {
+      });
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+  
+          <img src={logo} className="App-logo" alt="logo" />
+          <ClientInput handleCallBack = {this.handleCallBack} toggleReplaceJobs = {this.toggleReplaceJobs}/>
+          <JobDisplay 
+            companies = {this.state.companies}
+            searchlURLs = {this.state.searchlURLs}
+            jobs = {this.state.jobs}
+          />
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
