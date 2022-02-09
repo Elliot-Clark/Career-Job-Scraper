7988844f1dfb08b9d4aecb3f961ca6c6255a174f
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-08 19:40:10 -0700

    Optimization Code
    Rejects css and images from fetched websites for faster load times

--- routes/amazon.js
+++ routes/amazon.js
@@ -8,6 +8,15 @@ router.post('/jobSearch', function(req, res) {
 		(async () => {
 			const browser = await puppeteer.launch();
 			const page = await browser.newPage(); 
+			await page.setRequestInterception(true);
+			page.on('request', (req) => {
+				if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+				req.abort();
+				} else {
+				req.continue();
+				}
+			});
+
 			const URL = "https://www.amazon.jobs/en/search?offset=0&result_limit=10&sort=relevant&" 
 				+ (jobSearchInput.country ? 'country%5B%5D=' + jobSearchInput.country : '')
 				+ (jobSearchInput.USstate ? '&state%5B%5D=' + jobSearchInput.USstate.split(' ').join("%20") : '')
@@ -28,7 +37,6 @@ router.post('/jobSearch', function(req, res) {
 				}
 				return eleArr
 			});
-			console.log(jobTitles);
 
 			let jobLinks = await page.$$eval('.job-link', links => {
 				links = links.map(element => element.href)
@@ -38,7 +46,6 @@ router.post('/jobSearch', function(req, res) {
 				}
 				return eleArr
 			});
-			console.log(jobLinks);
 
 			let jobLocations = await page.$$eval('.location-and-id', links => {
 				links = links.map(element => element.textContent)
@@ -48,17 +55,15 @@ router.post('/jobSearch', function(req, res) {
 				}
 				return eleArr
 			});
-			console.log(jobLocations);
 
 			let results = ["Amazon", URL];
 			for (i = 0; i <jobTitles.length; i++) {
 				results.push([jobTitles[i], jobLinks[i], jobLocations[i]])
 			}
+			console.log(results);
 			res.json(results);
 		})();
 	});
-
-
 
 module.exports = router
 
