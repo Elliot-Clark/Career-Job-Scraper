bf7ea8fd018175701dd7375e368496ed245c8e37
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-09 23:14:36 -0700

    Adjusted

--- client/src/components/jobDisplay/jobDisplay.js
+++ client/src/components/jobDisplay/jobDisplay.js
@@ -3,26 +3,33 @@ import './jobDisplay.css';
 
 const JobDisplay = (props) =>  {
 
-    
     const jobDisplay = props.companies.map((item, index) => {
+
         return (
             <div className="groupedJobList" key={index}>
                 <a target="_blank" rel="noreferrer" className="companyTitle" href={props.searchlURLs[index]}>
                     <span>{item}</span>
                     <img src={require(`./logos/${item}.png`)} alt={item + " Logo"} ></img>
                 </a>
-                {
-                    props.jobs.map((item, index) => {
-                        if (!props.jobs[index][0]) {
-                            return ''
-                        }
+                
+                {   
+                    props.jobs[index].map((item, index) => {
+                        // if (!props.jobs[index]) {
+                        //     return ''
+                        // }
                         return (
                             <ul key={index}>
                                 <a target="_blank" rel="noreferrer" href={item[1]} >
                                     <li>
-                                        {item[0]}
-                                        <br></br>
-                                        {item[2]}
+                                        <div>
+                                            <span className="jobTitle">{item[0]}</span>
+                                            <br></br>
+                                            <span className="jobLocation">{item[2]}</span>
+                                            <br></br>
+                                        </div>
+                                        <div>
+                                            <span className="jobPostDate">{item[3]}</span>
+                                        </div>                                       
                                     </li>
                                 </a>
                             </ul>
