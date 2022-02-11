32b748fde560adfc276ff95fc82d87acd3239898
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-10 21:58:33 -0700

    Updated

--- client/src/components/clientInput/checkbox.js
+++ client/src/components/clientInput/checkbox.js
@@ -4,18 +4,11 @@ import './checkbox.css'
 
 let Checkbox = () => {
 
-    let test = () => {
-        let a = document.getElementById("Amazon").checked
-        let b = document.getElementById("Paypal").checked
-        console.log(a);
-        console.log(b);
-    }
-
     return (
         <div>
             <input type="checkbox" id="Amazon" name="Amazon"></input>
             <label htmlFor="Amazons"> Amazon</label>
-            <input type="checkbox" id="Paypal" name="Paypal" onChange={test}></input>
+            <input type="checkbox" id="Paypal" name="Paypal"></input>
             <label htmlFor="Paypals"> Paypal</label>
         </div>
     )
