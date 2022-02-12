1d90a49e95393d5fee595f5785cde01073e555bb
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-11 22:25:51 -0700

    Changed array name

--- routes/amazon.js
+++ routes/amazon.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/amazon', function(req, res) {
 	let jobSearchInput = req.query;
 		(async () => {
+			const companyName = "Amazon"
 			const browser = await puppeteer.launch();
 			const page = await browser.newPage(); 
 			await page.setRequestInterception(true);
@@ -16,7 +17,9 @@ router.post('/amazon', function(req, res) {
 				req.continue();
 				}
 			});
-
+			
+			
+			//Each URL has to be customized to fit each individual website
 			const URL = "https://www.amazon.jobs/en/search?offset=0&result_limit=10&sort=relevant&" 
 				+ (jobSearchInput.country ? 'country%5B%5D=' + jobSearchInput.country : '')
 				+ (jobSearchInput.USstate ? '&state%5B%5D=' + jobSearchInput.USstate.split(' ').join("%20") : '')
@@ -31,34 +34,51 @@ router.post('/amazon', function(req, res) {
 
 			let jobTitles = await page.$$eval('.job-title', links => {
 				links = links.map(element => element.textContent)
-				let eleArr = []
+				let arr = []
 				for (let i = 0; i < 5; i++) {
-					eleArr.push(links[i]);
+					arr.push(links[i]);
 				}
-				return eleArr
+				return arr
 			});
+
+			//Escape function if the scraped results return nothing from targeted site
+			if(!jobTitles[0]) {
+				let results = [companyName, URL, ["No Results. Note: This company's search works only for certain and exact city locations OR no City input entered at all"]]
+				res.json(results)
+				return
+			}
 
 			let jobLinks = await page.$$eval('.job-link', links => {
 				links = links.map(element => element.href)
-				let eleArr = []
+				let arr = []
 				for (let i = 0; i < 5; i++) {
-					eleArr.push(links[i]);
+					arr.push(links[i]);
 				}
-				return eleArr
+				return arr
 			});
 
 			let jobLocations = await page.$$eval('.location-and-id', links => {
 				links = links.map(element => element.textContent)
-				let eleArr = []
+				let arr = []
 				for (let i = 0; i < 5; i++) {
-					eleArr.push(links[i]);
+					arr.push(links[i]);
 				}
-				return eleArr
+				return arr
 			});
 
-			let results = ["Amazon", URL];
+			let jobPostDate = await page.$$eval('.posting-date', links => {
+				links = links.map(element => element.textContent)
+				console.log(links);
+				let arr = []
+				for (let i = 0; i < 5; i++) {
+					arr.push(links[i]);
+				}
+				return arr
+			});
+
+			let results = [companyName, URL, ""];
 			for (i = 0; i <jobTitles.length; i++) {
-				results.push([jobTitles[i], jobLinks[i], jobLocations[i]])
+				results.push([jobTitles[i], jobLinks[i], jobLocations[i], jobPostDate[i]])
 			}
 			console.log(results);
 			res.json(results);
@@ -66,85 +86,5 @@ router.post('/amazon', function(req, res) {
 	});
 
 module.exports = router
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// router.get('/jobSearch', (req, res) => {
-// 	console.log("Hello");
-// 	test.post('/jobSearch', function(req, res) {
-// 		let jobSearchInput = req.query;
-// 			(async () => {
-// 				const browser = await puppeteer.launch();
-// 				const page = await browser.newPage(); 
-// 				const URL = "https://www.amazon.jobs/en/search?offset=0&result_limit=10&sort=relevant&" 
-// 					+ (jobSearchInput.country ? 'country%5B%5D=' + jobSearchInput.country : '')
-// 					+ (jobSearchInput.USstate ? '&state%5B%5D=' + jobSearchInput.USstate.split(' ').join("%20") : '')
-// 					+ (jobSearchInput.city ? '&city%5B%5D=' + jobSearchInput.city.split(' ').join("%20") : '')
-// 					+ "&distanceType=Mi&radius=24km&latitude=&longitude=&loc_group_id=&loc_query="
-// 					+ "&base_query=" + jobSearchInput.jobTitleSearch.split(' ').join("%20")
-// 					+ "&city=&country=&region=&county=&query_options=&"
-// 				await page.goto(URL, {
-// 					waitUntil: "networkidle2",
-// 				});
-	
-	
-// 				let jobTitles = await page.$$eval('.job-title', links => {
-// 					links = links.map(element => element.textContent)
-// 					let eleArr = []
-// 					for (let i = 0; i < 5; i++) {
-// 						eleArr.push(links[i]);
-// 					}
-// 					return eleArr
-// 				});
-// 				console.log(jobTitles);
-	
-// 				let jobLinks = await page.$$eval('.job-link', links => {
-// 					links = links.map(element => element.href)
-// 					let eleArr = []
-// 					for (let i = 0; i < 5; i++) {
-// 						eleArr.push(links[i]);
-// 					}
-// 					return eleArr
-// 				});
-// 				console.log(jobLinks);
-	
-// 				let jobLocations = await page.$$eval('.location-and-id', links => {
-// 					links = links.map(element => element.textContent)
-// 					let eleArr = []
-// 					for (let i = 0; i < 5; i++) {
-// 						eleArr.push(links[i]);
-// 					}
-// 					return eleArr
-// 				});
-// 				console.log(jobLocations);
-	
-// 				let results = ["Amazon", URL];
-// 				for (i = 0; i <jobTitles.length; i++) {
-// 					results.push([jobTitles[i], jobLinks[i], jobLocations[i]])
-// 				}
-// 				res.json(results);
-// 			})();
-// 	  });
-// })
-
-
-
 
 
