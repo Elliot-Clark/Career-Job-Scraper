f3ca11c72c06df3a20dd17390533877a12480191
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-11 22:25:17 -0700

    Added additional companies

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const app = express();
 
-const routers = require('./routes/amazon.js');
-app.use(routers);
+const adobe = require('./routes/adobe.js');
+app.use(adobe)
 
-const router = require('./routes/paypal.js');
-app.use(router)
+const amazon = require('./routes/amazon.js');
+app.use(amazon);
+
+const paypal = require('./routes/paypal.js');
+app.use(paypal)
+
+const apple = require('./routes/apple.js');
+app.use(apple)
+
+const uber = require('./routes/uber.js');
+app.use(uber)
+
+const crateandbarrel = require('./routes/crateandbarrel.js');
+app.use(crateandbarrel)
+
+const ebay = require('./routes/ebay.js');
+app.use(ebay)
+
+const microsoft = require('./routes/microsoft.js');
+app.use(microsoft)
+
+const facebook = require('./routes/facebook.js');
+app.use(facebook)
+
+const sap = require('./routes/sap.js');
+app.use(sap)
+
+const intuit = require('./routes/intuit.js');
+app.use(intuit)
+
+const shopify = require('./routes/shopify.js');
+app.use(shopify)
+
+
+
 
 
 
 
 const port = 5000;
-
 app.listen(port, () => `Server running on port ${port}`);
