e7f5b7cd2f49f7b49ab06b16c42d9ef2f873dabf
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2022-02-25 19:08:21 -0700

    Footer Added

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import './footer.css'
+
+let Footer = () => {
+    return (
+        <div id="footer">
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/elliot-clark-05002a106/">Developed by Elliot Clark </a>
+            <a target="_blank" rel="noreferrer" href="https://github.com/Elliot-Clark/Career-Job-Scraper">Github Link</a>
+        </div>
+    )
+}
+
+export default Footer
